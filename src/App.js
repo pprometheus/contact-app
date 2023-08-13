e028d7f683b05c639610533d415a234b56c6e154
pprometheus <67922206+pprometheus@users.noreply.github.com>
2023-08-13 20:52:15 +0530

    Updated Component files

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
 
 function App() {
+
+  const contacts =[
+  {
+    id: "1",
+    name: "Amith Paul",
+    email: "amithhatwar@gmail.com",
+  },
+  {
+    id: "2",
+    name: "Easha",
+    email: "e@gmail.com",
+  }
+];
   return (
-    <div>
+    <div className='ui container'>
     <Header/>
     <AddContact/>
    
-    <ContactList/>
+    <ContactList contacts={contacts}/>
     </div>
  
 
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 
-const ContactList = () => {
+const ContactList = (props) => {
+    console.log(props);
+
+    const renderContatList = props.contacts.map((contact) =>{
+        return(
+        <ContactCard contact={contact}/>
+        );
+    });
+
     return(
         <div className="ui celled list">
-           ContactList
+          {renderContatList}
         </div>
     );
 } 
